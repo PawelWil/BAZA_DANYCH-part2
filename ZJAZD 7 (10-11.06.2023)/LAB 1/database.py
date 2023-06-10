0bddd492653fafaac963230d42980ee8bab26eff
wiltos <pawel.wiltos@gmail.com>
2023-06-10 15:44:40 +0200

    update

--- ZJAZD 7 (10-11.06.2023)/LAB 1/database.py
+++ ZJAZD 7 (10-11.06.2023)/LAB 1/database.py
@@ -1,6 +1,13 @@
 from ingredient import Ingredient
 from connection import connection
 
+def add_many_ingredients(ingredients):
+    with connection.cursor() as cursor:
+        cursor.fast_executemany = True
+        cursor.executemany("""
+        INSERT INTO ingredients(ingredient_name, calories, protein, fat, carbs, fiber, ingredient_type)
+        VALUES(?, ?, ?, ?, ?, ?, ?)
+        """, ingredients)
 
 def add_ingredient(name, calories, protein, fat, carbs, fiber, ingredient_type) -> None:
     with connection.cursor() as cursor:
--- ZJAZD 7 (10-11.06.2023)/LAB 1/loader.py
+++ ZJAZD 7 (10-11.06.2023)/LAB 1/loader.py
@@ -6,5 +6,7 @@ def load_initial_data(filename='ingredients.csv') -> None:
     with open(filename, 'r', encoding='UTF-8', newline='') as ingredients_file:
         reader = csv.reader(ingredients_file, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
         ingredients_file.readline()  # Ignore headers
+        ingredients = []
         for row in reader:
             database.add_ingredient(*row)
+        database.add_many_ingredients(ingredients)
